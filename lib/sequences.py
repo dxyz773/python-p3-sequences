21d42760cad4d8ad87b2e5f9b647be159786a2c8
demitryedwards <dxyz773@gmail.com>
2023-05-22 20:47:22 -0400

    painfully completed lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0, 1])
+    else:
+        num = 0
+        prev = 0
+        current = 1
+        my_fib = [prev, current]
+        while num < length:
+            moment = prev + current
+            my_fib.append(moment)
+            prev = current
+            current = moment
+            num += 1
+        if length <= 10:
+            print(my_fib[0:10])
+        else:
+            print(my_fib)
+
+
+print_fibonacci(10)
